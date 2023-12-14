6df46947226165964a30aca57ae662735e53cb95
“ponsAoki” <aokishogo20pk@gmail.com>
2023-12-14 14:54:03 +0900

    fix: html表示方法修正

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.use(express.static("."));
+app.use(express.static("./game"));
 
 app.get("/", (_, res) => {
-  res.redirect("/game/main.html");
+  res.sendFile(path.join(__dirname, "/game/main.html"));
 });
 
 app.listen(3000, () => {
